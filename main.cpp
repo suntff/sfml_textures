a6e3490bd075fb9abcc105258772aac238421a03
suntff <morakindaniil@gmail.com>
2024-04-26 21:57:58 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,13 @@
 ﻿#include <SFML/Graphics.hpp>
 #include <Game.hpp>
 #include <Ship.hpp>
+#include <iostream>
 int main()
 {
-    srand(time(nullptr));
-    msp::Game game(1000, 600, "Game");
-
-    if (!game.Setup(10))
+    msp::Game game(1000,500,40,40,"sfml_textures");
+    if (game.Texture_setup() == -1) {
         return -1;
-
-    game.LifeCycle();
-
-    return 0;
+    }
+    srand(time(nullptr));
+    game.Life_cycle();
 }
