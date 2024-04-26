57d0796e168884b613b6848714c5a583d2f146d0
suntff <morakindaniil@gmail.com>
2024-04-26 21:57:55 +0200

    Update Ship.hpp

--- inc/Ship.hpp
+++ inc/Ship.hpp
@@ -1,61 +1,15 @@
 ﻿#pragma once
 #include <SFML/Graphics.hpp>
-#include <Game.hpp>
 #include <iostream>
 namespace msp {
-	class Ship
-	{
-		const float pi = acos(-1);
-		float x, y;
-		float alpha;
-		float v;
-		sf::Texture textureShip;
-		sf::Sprite spriteShip;
-
+	class Ship {
+		sf::Texture texture_ship; sf::Sprite sprite_ship; float x; float y; float alfa; const long double pi = acos(-1);
 	public:
+		int Ship_setup(float x, float y);
+		void Ship_rotate(float dalfa, float dt);
+		void Update_ship_position(float dv, float dt);
 		
-		bool Setup(float x, float y)
-		{
-			this->x = x;
-			this->y = y;
-			alpha = 0;
-			v = 0;
-			if (!textureShip.loadFromFile("assets\\ship.png"))
-			{
-				std::cout << "Error while loading ship.png" << std::endl;
-				return false;
-			}
-			spriteShip.setTexture(textureShip);
-			spriteShip.setScale(0.3f, 0.3f);	
-			spriteShip.setOrigin(textureShip.getSize().x / 2, textureShip.getSize().y / 2);
-			spriteShip.setPosition(x, y);
-			spriteShip.setRotation(90);
-			spriteShip.rotate(alpha);
-			
-		}
-
-		void setVelocity(float dv)
-		{
-			v += dv;
-		}
-
-		void Move()
-		{
-			float alphaRad = acos(-1) * alpha / 180;
-			x += v * cos(alphaRad);
-			y += v * sin(alphaRad);
-			spriteShip.setPosition(x, y);
-		}
-
-		void Rotate(float dalpha)
-		{
-			alpha += dalpha;
-			spriteShip.setRotation(alpha);
-		}
-
-		sf::Sprite Get()
-		{
-			return spriteShip;
-		}
+		sf::Sprite Get_sprite_ship();
+		long double Degrees_to_rad(int alfa);
 	};
 }
